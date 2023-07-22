3998e19a1a77597f3789560e0b0e2d0606c10a4a
Henrique Zimermann <henriquezimermanngeo@gmail.com>
2023-07-22 10:26:26 -0300

    ataualizando

---
+++ ComponentesComunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuario: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            //required: true,
+            //default: function() {
+                //return Array(10).fill(0).join(',');
+            //}
+            default: 'Anonimo',
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('');
+        },
+        reiniciarNome() {
+            const antigo = this.nome
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', {
+                novo: this.nome,
+                antigo
+                });
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
